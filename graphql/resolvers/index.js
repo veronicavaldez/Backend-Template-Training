bad6ba7187eb9dfa4320978c74b8dc257fc72f4e
Hector Borjas <105574084+HectorB06@users.noreply.github.com>
2025-02-26 09:24:17 -0500

    Update index.js

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,3 +1,33 @@
+/*
+
+# 
+the code below will be changed to whichever name your type is, but the structure will stay the same!
+Ex:
+
 const artistsResolvers = require('./artists');
 
 module.exports = artistsResolvers;
+
+if you have multiple types, it might look a little more like the following:
+
+const postsResolvers = require('./posts');
+const usersResolvers = require('./users');
+const commentsResolvers = require('./comments')
+
+module.exports = {
+    Post: {
+        likeCount: (parent) => parent.likes.length,
+        commentCount: (parent) => parent.comments.length
+      },
+    Query: {
+
+        ...postsResolvers.Query
+    },
+    Mutation: {
+        ...usersResolvers.Mutation,
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
+    }
+
+};
+*/
